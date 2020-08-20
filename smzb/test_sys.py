72923bec1253ff82db2890fbf131f2fa7047d3f7
Flury <1012883981@qq.com>
2020-08-20 15:54:07 +0800

    Update test_sys.py

--- smzb/test_sys.py
+++ smzb/test_sys.py
@@ -8,13 +8,13 @@ from locust import HttpUser, TaskSet, task, between, User
 class Main(TaskSet):
     @task(1)
     def to_main(self):
-        url = "/ofm"
+        url = "/"
         self.client.get(url, name="打开系统登陆页面")
 
 class MainUser(HttpUser):
     tasks = {Main: 2}
-    wait_time = between(1000, 3000)
-    host = "http://10.192.30.232/"
+    wait_time = between(1, 3)
+    host = "http://www.baidu.com"
 
 
 if __name__ == '__main__':
